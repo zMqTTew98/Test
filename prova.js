39f84232fbfd1c7055e9657e5430ca6da0a7fc3a
unknown <mettewgithub@gmail.com>
2025-04-02 10:05:00 +0200

    Programma Aggiornato

--- prova.js
+++ prova.js
@@ -1,15 +1,16 @@
 import PromptSync from "prompt-sync";
 const input=PromptSync();
 
-const concatenaParole=(a,b)=>{
-    return a+b;
+const concatenaParole=(a,b,c)=>{
+    return a+b+c;
 }
 
 function main(){
     let parola1=input("Inserisci la prima parola: ");
     let parola2=input("Inserisci la seconfa parola: ");
+    let parola3=input("Inserisci la terza parola: ");
 
-    let parolaConcatenata=concatenaParole(parola1,parola2);
+    let parolaConcatenata=concatenaParole(parola1,parola2,parola3);
     console.log("La parola concatenata e': "+parolaConcatenata);
 }
 
